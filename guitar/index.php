8bbae612ceaa9523cbd4080ad79c39582d1a6d61
Seybel <seyibello31@gmail.com>
2019-03-04 09:35:15 +0100

    Sorted the highscore list

--- guitar/index.php
+++ guitar/index.php
@@ -1,46 +1,52 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>Guitar Wars - High Scores</title>
-  <link rel="stylesheet" type="text/css" href="style.css" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Guitar Wars - High Scores</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
+
 <body>
-  <h2>Guitar Wars - High Scores</h2>
-  <p>Welcome, Guitar Warrior, do you have what it takes to crack the high score list? If so, just <a href="uploadscore.php">add your own score</a>.</p>
-  <hr />
+    <h2>Guitar Wars - High Scores</h2>
+    <p>Welcome, Guitar Warrior, do you have what it takes to crack the high score list? If so, just <a href="uploadscore.php">add your own score</a>.</p>
+    <hr />
 
-<?php
+    <?php
 
 
-  // Connect to the database 
-require_once('db_connect.php');
-require_once('app_vars.php');
+    // Connect to the database 
+    require_once('db_connect.php');
+    require_once('app_vars.php');
 
-$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
-  // Retrieve the score data from MySQL
-$query = "SELECT * FROM guitar_wars";
-$data = mysqli_query($dbc, $query);
+    // Retrieve the score data from MySQL
+    $query = "SELECT * FROM guitar_wars ORDER BY score DESC, date ASC";
+    $data = mysqli_query($dbc, $query);
 
-  // Loop through the array of score data, formatting it as HTML 
-  echo '<table>';
-while ($row = mysqli_fetch_array($data)) { 
-    // Display the score data
-    echo '<tr><td class="scoreinfo">';
-    echo '<span class="score">' . $row['score'] . '</span><br />';
-    echo '<strong>Name:</strong> ' . $row['name'] . '<br />';
-    echo '<strong>Date:</strong> ' . $row['date'] . '</td>';
-    
-    if (is_file($row['screenshot']) && filesize($row['screenshot']) > 0) {
-      echo '<td><img src="' . GW_UPLOADPATH . $row['screenshot'] . '" alt="Score Image" /></td></tr>'; 
-    } 
-    else {
-      echo '<td><img src="' . 'images/unverified.gif' . '" alt="Unverified score" /></td></tr>';
+    // Loop through the array of score data, formatting it as HTML 
+    echo '<table>';
+    $i = 0;
+    while ($row = mysqli_fetch_array($data)) {
+      // Display the score data
+      if ($i == 0) {
+        echo '<tr><td colspan="2" class="topscoreheader">Top score:' . $row['score'] . '</td></tr>';
+      }
+      echo '<tr><td class="scoreinfo">';
+      echo '<span class="score">' . $row['score'] . '</span><br />';
+      echo '<strong>Name:</strong> ' . $row['name'] . '<br />';
+      echo '<strong>Date:</strong> ' . $row['date'] . '</td>';
+      // die(var_dump(__DIR__ . '/' . ($row['screenshot'])));
+      if (isset($row['screenshot'])) {
+        echo '<td><img src="' . $row['screenshot'] . '" alt="Score Image" /></td></tr>';
+      } else {
+        echo '<td><img src="' . 'images/unverified.gif' . '" alt="Unverified score" /></td></tr>';
+      }
+      $i++;
     }
-}
-  echo '</table>';
+    echo '</table>';
 
-mysqli_close($dbc);
-?>
+    mysqli_close($dbc);
+     ?>
--- guitar/uploadscore.php
+++ guitar/uploadscore.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>Guitar Wars - Add Your High Score</title>
-  <link rel="stylesheet" type="text/css" href="style.css" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Guitar Wars - Add Your High Score</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
 
-  <style>
+    <style>
 
-      body {
+        body {
           max-width: 800px;
           min-width: 400px;
           margin: 50px;
@@ -27,14 +28,14 @@
 
 <body>
 
-  <h2 class="lead">Guitar Wars - Add Your High Score</h2>
+    <h2 class="lead">Guitar Wars - Add Your High Score</h2>
 
-<?php
+    <?php
 
-require_once('app_vars.php');
-require_once('db_connect.php');
+    require_once('app_vars.php');
+    require_once('db_connect.php');
 
-  if (isset($_POST['submit'])) {
+    if (isset($_POST['submit'])) {
       // Grab the score data from the POST
       $name = $_POST['name'];
       $score = $_POST['score'];
@@ -45,54 +46,55 @@ require_once('db_connect.php');
         //Move the file to the target upload folder
         $target = GW_UPLOADPATH . time() . $screenshot;
 
-        if (move_uploaded_file($_FILES['screenshot']['tmp_name'], $target)) {  
-        // Connect to the database
-      $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-        
-        // Write the data to the database
-          $query = "INSERT INTO guitar_wars VALUES (0, NOW(), '$name', '$score', '$screenshot')";
+        if (move_uploaded_file($_FILES['screenshot']['tmp_name'], $target)) {
+          echo "The file " . $screenshot . " has been uploaded.";
+
+          // Connect to the database
+          $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+          // Write the data to the database
+          $query = "INSERT INTO guitar_wars VALUES (0, NOW(), '$name', '$score', '$target')";
           mysqli_query($dbc, $query);
 
-        // Confirm success with the user
+          // Confirm success with the user
           echo '<p>Thanks for adding your new high score!</p>';
           echo '<p><strong>Name:</strong> ' . $name . '<br />';
-          echo '<strong>Score:</strong> ' . $score . '</p>';
-          echo '<img src ="' . GW_UPLOADPATH . $screenshot . '" alt="Score Image"' . '<br/>' ;
+          echo '<strong>Score:</strong> ' . $score . '</p>' . '<br/>';
           echo '<p><a href="index.php">&lt;&lt; Back to high scores</a></p>';
 
-        // Clear the score data to clear the form
+          // Clear the score data to clear the form
           $name = "";
           $score = "";
           $screenshot = "";
 
           mysqli_close($dbc);
-        } 
+        }
       }
 
       // if (!is_numeric($score)) {
       //   echo '<p class="error">Please enter a number as your score.</p>';
       // }
 
-    else {
+      else {
         echo '<p class="error">Please enter all of the information to add your high score.</p>';
+      }
     }
-  }
 
-?>
+    ?>
 
-  <hr />
-  <form enctype="multipart/form-data" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-    <input type="hidden" name="MAX_FILE_SIZE" value="100000" />
-    <label for="name">Name:</label>
-    <input type="text" id="name" name="name" value="<?php if (!empty($name)) echo $name; ?>" /><br />
-    <label for="score">Score:</label>
-    <input type="text" id="score" name="score" value="<?php if (!empty($score)) echo $score; ?>" /><br />
-    <label for="screenshot">Image:</label>
-    <input type="file" name="screenshot" id="screenshot">
-  <hr />
-    <input type="submit" value="Add" name="submit" />
-  </form>
+    <hr />
+    <form enctype="multipart/form-data" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <input type="hidden" name="MAX_FILE_SIZE" value="100000" />
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" value="<?php if (!empty($name)) echo $name; ?>" /><br />
+        <label for="score">Score:</label>
+        <input type="text" id="score" name="score" value="<?php if (!empty($score)) echo $score; ?>" /><br />
+        <label for="screenshot">Image:</label>
+        <input type="file" name="screenshot" id="screenshot">
+        <hr />
+        <input type="submit" value="Add" name="submit" />
+    </form>
 
-</body> 
-</html>
+</body>
 
+</html >
